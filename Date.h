e9c05d15b52f43dbdc85a20940eef24b92ed48c8
Gal Warsulker <159069893+Varsul17@users.noreply.github.com>
2024-02-19 17:41:38 +0200

    Create Date.h

---
+++ Date.h
@@ -0,0 +1,29 @@
+#ifndef FIELDSPROJECT_DATE_H
+#define FIELDSPROJECT_DATE_H
+#include <iostream>
+
+using namespace std;
+
+class Date {
+private:
+    int year;
+    int month;
+    int day;
+
+public:
+    explicit Date(int n_year = 0, int n_month = 0, int n_day = 0): year(n_year), month(n_month), day(n_day) {}
+    Date(const Date& copy){this->day = copy.day; this->month = copy.month; this->year = copy.year;}
+//    int get_day() const {return this->day;}
+//    int get_month() const {return this->month;}
+//    int get_year() const {return this.year;};
+
+
+    friend ostream& operator<<(ostream& os, const Date& obj){
+        os << obj.day << "\\" << obj.month << "\\" << obj.year << endl;
+        return os;
+    }
+
+};
+
+
+#endif //FIELDSPROJECT_DATE_H
